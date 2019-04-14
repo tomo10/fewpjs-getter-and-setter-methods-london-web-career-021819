965c6368c0bcd9846781512ccbf17cce497f3fd7
Thomas Edwards <thce28@gmail.com>
2019-04-14 11:11:36 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your Circle class here
+class Circle {
+    constructor(radius) {
+        this.radius = radius 
+    }
+
+    get diameter() {
+        return this.radius * 2
+    }
+
+    get circumference() {
+        return this.radius * 2 * Math.PI 
+    }
+
+    get area() {
+        return (this.radius * this.radius) * (Math.PI)
+    }
+
+    set diameter(newDiameter) {
+        return this.radius = newDiameter / 2 
+    }
+
+    set circumference(newCircumference) {
+        return this.radius = newCircumference / Math.PI / 2
+    }
+    
+}
